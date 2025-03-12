58209f3154460d898ff1b64d59dbcdfa2f8fc062
Alberto Aragón Peci <alberto.aragon.peci@gmail.com>
2025-03-12 19:09:06 +0100

    chore: Load project code

---
+++ JPApexPredators17/ApexPredator.swift
@@ -0,0 +1,72 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators17
+//
+//  Created by Alberto Aragón Peci on 21/4/24.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+struct ApexPredator: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let type: PredatorType
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    struct MovieScene: Decodable, Identifiable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+}
+
+enum PredatorType: String, Decodable, CaseIterable, Identifiable {
+    case all
+    case land       // land
+    case air        // air
+    case sea        // sea
+  
+    var id: PredatorType {
+        self
+    }
+    
+    var background: Color {
+        switch self {
+        case .land:
+                .brown
+        case .air:
+                .teal
+        case .sea:
+                .blue
+        case .all:
+                .black
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .all:
+            "square.stack.3d.up.fill"
+        case .land:
+            "leaf.fill"
+        case .air:
+            "wind"
+        case .sea:
+            "drop.fill"
+        }
+    }
+}
---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Alberto Aragón Peci on 21/4/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
